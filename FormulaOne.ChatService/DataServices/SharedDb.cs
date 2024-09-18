b007d04baebf5b07772e073e79b67ca8f8ab8e85
phathuynh2082002 <142472879+phathuynh2082002@users.noreply.github.com>
2024-09-18 22:56:31 +0700

    commit backend

---
+++ FormulaOne.ChatService/DataServices/SharedDb.cs
@@ -0,0 +1,12 @@
+﻿using FormulaOne.ChatService.Models;
+using System.Collections.Concurrent;
+
+namespace FormulaOne.ChatService.DataServices
+{
+    public class SharedDb
+    {
+        public readonly ConcurrentDictionary<string, UserConnection> _connections = new();
+        public ConcurrentDictionary<string, UserConnection> connections => _connections;
+
+    }
+}
---
+++ FormulaOne.ChatService/Program.cs
@@ -0,0 +1,56 @@
+
+using FormulaOne.ChatService.DataServices;
+using FormulaOne.ChatService.Hubs;
+
+namespace FormulaOne.ChatService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddSignalR();
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("reactApp",builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
+
+            builder.Services.AddSingleton<SharedDb>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.MapHub<ChatHub>("/Chat");
+
+            app.UseCors("reactApp");
+
+            app.Run();
+        }
+    }
+}
